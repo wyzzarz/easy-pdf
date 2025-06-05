b095a010624788d47e549935166875ed8bc46814
Warner Zee <warner@zoynk.com>
2025-06-05 08:24:42 -0700

    Create rust library

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2025 Warner Zee <warner@zoynk.com>
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
