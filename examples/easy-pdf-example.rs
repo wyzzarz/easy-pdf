f6f9a3252af6be0b7db9c224794de11cf5b8b1ac
Warner Zee <warner@zoynk.com>
2025-06-05 10:17:14 -0700

    Add example

---
+++ examples/easy-pdf-example.rs
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2025 Warner Zee <warner@zoynk.com>
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+use easy_pdf::hello_world;
+
+fn main() {
+    if let Err(e) = example() {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }   
+}
+
+fn example() -> Result<(), Box<dyn std::error::Error>> {
+    hello_world()
+}
