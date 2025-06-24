3cee7aaa1f71347e68f019df57f9b5b6405bb6f1
Warner Zee <warner@zoynk.com>
2025-06-23 22:36:02 -0700

    Add insert_content and get_content to contents

--- src/content/contents.rs
+++ src/content/contents.rs
@@ -4,7 +4,7 @@
 use rand::Rng;
 use std::collections::HashMap;
 use std::sync::{LazyLock, Mutex};
-use super::{Content, ContentId};
+use super::{Content, ContentId, Layout};
 
 /// Holds an instance of all contents.
 pub(crate) static CONTENTS: LazyLock<Mutex<Contents>> = LazyLock::new(|| Mutex::new(Contents::new()));
@@ -38,6 +38,19 @@ impl Contents {
         }
     }
 
+    /// Gets content by id.
+    fn get_content(&self, id: ContentId) -> Option<Content> {
+        self.contents
+            .get(&id)
+            .map(|content| content.clone())
+    }
+
+    // Inseerts the content.
+    fn insert_content(&mut self, content: Content) -> Option<Content> {
+        let id = content.get_id();
+        self.contents.insert(id, content)
+    }
+
 }
 
 /// Generates a new content id.
@@ -45,14 +58,36 @@ pub fn new_content_id() -> ContentId {
     CONTENTS.lock().unwrap().new_content_id()
 }
 
+/// Gets content by id.
+pub fn get_content(id: ContentId) -> Option<Content> {
+    CONTENTS.lock().unwrap().get_content(id)
+}
+
+/// Inserts the content.
+pub fn insert_content(content: Content) -> Option<Content> {
+    CONTENTS.lock().unwrap().insert_content(content)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::content::Frame;
+    use crate::geometry::RECT0;
 
     #[test]
     fn test_new_content_id() {
         let contents = Contents { contents: HashMap::new() };
         assert_ne!(contents.new_content_id(), contents.new_content_id());
+    }
+
+    #[test]
+    fn test_insert_and_get_content() {
+        let content_id = 123;
+        let frame = Frame::new(content_id, RECT0);
+        let content = Content::Frame(frame);
+        assert!(insert_content(content.clone()).is_none());
+        let content1 = get_content(content_id).unwrap();
+        assert_eq!(content, content1);
     }
 
 }
